f1a0655c91489111a99b46926f5bd4914737a24b
sai <maks.utah@gmail.com>
2015-06-03 16:37:39 +0300

    2

--- upload.php
+++ upload.php
@@ -1,9 +1,11 @@
 <?php
 require_once ("config.php");
-$uploaddir = 'tmp/';
-$uploadfile = '/var/www/html/alaskan/uploads/' . basename($_FILES['userfile']['tmp_name']);
+$uploaddir = 'C:\Windows\Temp';
+//$uploadfile = '/var/www/html/alaskan/uploads/' . basename($_FILES['userfile']['tmp_name']);
+$uploadfile = 'D:/www/alyaska/uploads/' . basename($_FILES['userfile']['tmp_name']);
 $filename = basename($_FILES['userfile']['tmp_name']);
-echo $filename;
+print_r($filename);
+echo $uploadfile;
 echo '<pre>';
 if ($_FILES['userfile']['type'] !== "image/jpeg") {header("Location: index.php?error=1"); exit();}
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
@@ -23,10 +25,10 @@ echo 'Некоторая отладочная информация:';
 print_r($_FILES);
 
 print "</pre>";
-header('Content-type: image/jpeg');
-$image = new Imagick("uploads/$filename");
+//header('Content-type: image/jpeg');
+$image = new Imagick("$uploadfile");
 $image->thumbnailImage(0,80);
 $image->writeImage("uploads/preview/$filename");
 //include (blocks/image_convert.php);
-header("Location: index.php");
+//header("Location: index.php");
 ?>
